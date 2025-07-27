1db1a8ccf85ce832d615e7f6c6d766a6b5f8a9f3
yannickRafael <yannickrafael286@gmail.com>
2025-07-27 15:54:36 +0200

    added comments and refracotred code

--- helloworld/helloworld.s
+++ helloworld/helloworld.s
@@ -1,16 +1,17 @@
 section	.text
-   global _start
+global _start
 	
 _start:
-   mov	edx,len
-   mov	ecx,msg
-   mov	ebx,1
-   mov	eax,4
-   int	0x80
+	mov eax,0x4                                                     ; uses write system call(4)
+	mov ebx,1                                                       ; file descriptor for stdout(1)	
+	mov ecx,msg							; memory address to the data to be written
+	mov edx,len							; memory address to the lenght of the data to be written		
 	
-   mov	eax,1
-   int	0x80
+	int 0x80							; invokes a sys_call
+	mov eax,0x1
+	int 0x80
+	
 
 section	.data
-	msg db 'Hello, world! Yannick', 0xa
+	msg db 'Hello, world!',0xa,'I understand it, now!', 0xa
 	len equ $ - msg
