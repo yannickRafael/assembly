b332b2f42d85fa294f32acfc718920a3aef6edf7
yannickRafael <yannickrafael286@gmail.com>
2025-07-27 15:14:50 +0200

    minor changes

--- helloworld/helloworld.s
+++ helloworld/helloworld.s
@@ -1,16 +1,16 @@
-section .data
-	msg db 'Hello World!', 0xa
-	len equ $ - msg
-
-section .text
-global _start
-
+section	.text
+   global _start
+	
 _start:
-	mov edx,len
-	mov ecx,msg
-	mov ebx,1
-	mov eax,4
-	int 0x80
+   mov	edx,len
+   mov	ecx,msg
+   mov	ebx,1
+   mov	eax,4
+   int	0x80
+	
+   mov	eax,1
+   int	0x80
 
-	mov eax,1
-	int 0x80
+section	.data
+	msg db 'Hello, world! Yannick', 0xa
+	len equ $ - msg
