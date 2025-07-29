7afdc78980aac6aead3b1107f54a2bd6ae168c55
yannickRafael <yannickrafael286@gmail.com>
2025-07-29 14:12:00 +0200

    added more options

--- calculator/calculator.s
+++ calculator/calculator.s
@@ -3,9 +3,12 @@
 ; Author: Yannick Rafael(yannickRafael)
 
 section .data
-	optmsg db 'Select one operation below: ', 0xa, '1. Addition;', 0xa					; definfing the options prompt
+	optmsg db 'Select one operation below: ', 0xa, '1. Addition;', 0xa, '2. Subtraction', 0xa, '3. multiplication', 0xa, '4. Division',0xa, '5. Exit Program',0xa					; definfing the options prompt
 	optmsgsize equ $-optmsg										; computing the size of the message
 
+
+section .bss
+	option resb 3											; created uninitialized variable to store option		
 
 section .text
 global _start
@@ -18,6 +21,13 @@ _start:
 
 	mov eax,4											; defining the system call we want: 4 is for writing
 	int 80h												; invoking the sys call
+
+	mov ecx,option											; memory address to store the inserted value
+	mov edx,3											; maximum number of bytes to read
+	mov ebx,0											; selecting file descriptor 0 for stdin
+
+	mov eax,3											; selecting sys call number 3: read
+	int 80h												; invoking sys call
 
 	mov eax,1											; preparing graceful program exit
 	int 80h												; exiting the program gracefully
